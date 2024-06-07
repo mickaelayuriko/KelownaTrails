74d313dcc17029a42f0d762d74b0683910f69b8c
Mickaela Yuriko <mickaelayuriko@hotmail.com>
2024-06-06 20:50:52 -0700

    testing

--- test/test1.js
+++ test/test1.js
@@ -19,27 +19,31 @@ async function test_case() {
         //Grab an element from the page
         await driver.findElement(By.id("lastname")).click()
         // 4 | type | id=lastname | valencia
-        await driver.findElement(By.id("lastname")).sendKeys("valencia")
+        await driver.findElement(By.id("lastname")).sendKeys("Pulcherio")
         // 5 | click | id=firstname | 
         await driver.findElement(By.id("firstname")).click()
         // 6 | type | id=firstname | washington
-        await driver.findElement(By.id("firstname")).sendKeys("washington")
+        await driver.findElement(By.id("firstname")).sendKeys("Mickaela")
         // 7 | click | css=td > p:nth-child(4) | 
         await driver.findElement(By.css("td > p:nth-child(4)")).click()
         // 8 | type | id=GroupSize | 10
-        await driver.findElement(By.id("GroupSize")).sendKeys("10")
+        await driver.findElement(By.id("GroupSize")).sendKeys("5")
         // 9 | click | id=addMemberBtn | 
         await driver.findElement(By.id("addMemberBtn")).click()
 
         //Check the result
 
-        let countMembers = len(WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//select[@id='members']//option"))))
-
-        if (countMembers > 0) {
-            console.log('Test Success');
-        } else {
-            console.log('Test Failed');
+        // Wait for the members to be added and get the member names from the table
+        let memberOptions = await driver.wait(until.elementsLocated(By.xpath("//select[@id='members']//option")), 20000);
+        let memberNames = [];
+        for (let option of memberOptions) {
+            memberNames.push(await option.getText());
         }
+
+        // Check if the member names contain the exact name you entered
+        assert(memberNames.includes("Pulcherio Mickaela"), "Test Successd");
+
+        console.log('Test Failed');
     } catch (error) {
         console.log('An error accurred:', error);
     } finally {
